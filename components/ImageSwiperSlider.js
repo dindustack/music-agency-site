a9b1a5f33a9a581ca6b697d3fbc2dc691deaedfc
Chinwendu Agbaetuo <silviachinwendu@gmail.com>
2023-03-26 23:35:26 +0100

    swiper slider fix 1

---
+++ components/ImageSwiperSlider.js
@@ -0,0 +1,57 @@
+import Image from "next/image";
+import React, { useEffect, useState } from "react";
+
+import { Swiper, SwiperSlide } from "swiper/react";
+
+/**
+ *
+ * @returns Images
+ */
+import imageSlider1 from "../images/imageSlider1.png";
+import imageSlider2 from "../images/imageSlider2.png";
+import imageSlider3 from "../images/imageSlider3.png";
+// import imageSlider4 from "../images/imageSlider4.png"
+import imageSlider5 from "../images/imageSlider5.png";
+import imageSlider6 from "../images/imageSlider6.png";
+
+export default function ImageSwiperSlider() {
+  const [slides, setSlides] = useState(0);
+
+  useEffect(() => {
+    const width = window.innerWidth;
+
+    const calculatedWidth = width;
+
+    calculatedWidth < 768 ? setSlides(1.2) : setSlides(1.5);
+  }, []);
+
+  return (
+    <div className="relative">
+      <Swiper
+        spaceBetween={50}
+        slidesPerView={slides}
+        grabCursor={true}
+        className="w-full font-neutralFace h-[50vh] md:h-[65vh] relative mt-[40vh] md:mt-[33vh]"
+      >
+        <SwiperSlide className="flex justify-start flex-col w-fit">
+          <div className="h-[100%] flex items-end">
+            <Image
+              alt=""
+              src={imageSlider1}
+              className="w-full md:w-[80%] h-[100%] object-contain"
+            />
+          </div>
+          <div className="flex justify-between items-end h-[100%]">
+            <div className="flex flexcol justify-between h-full py-1 px-2">
+              <p className="uppercase">multiple</p>
+              <p>(01)</p>
+            </div>
+            <div className="text-5xl font-bold md:text-7xl lg:text-9xl 2xl:text-[12rem] leading-none uppercase">
+              malta
+            </div>
+          </div>
+        </SwiperSlide>
+      </Swiper>
+    </div>
+  );
+}
--- pages/index.js
+++ pages/index.js
@@ -1,10 +1,12 @@
 import React from "react";
 import Header from "../components/Header";
+import ImageSwiperSlider from "../components/ImageSwiperSlider";
 
 export default function HomePage() {
   return (
     <div className="text-[#e7e7e7] uppercase">
       <Header />
+      <ImageSwiperSlider />
     </div>
   );
 }
